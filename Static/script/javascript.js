ece52af53d12d4669ee0c301f15f29647e6404fe
Luzivam Pereira <luzivam.santos28@outlook.com>
2024-06-20 16:29:53 -0300

    modifiquei os arquivos Javascript.html , javascript.css e javascript.js

---
+++ Static/script/javascript.js
@@ -0,0 +1,27 @@
+
+
+document.getElementById('vam').innerHTML = 'My fisrt javaScript'
+
+function onLight(){
+document.getElementById('myimagem').src="../../Static/imgs/pic_bulbon.gif"
+}
+
+function offLight(){
+    document.getElementById('myimagem').src="../../Static/imgs/pic_bulboff.gif"
+}
+
+function fontSize(){
+    document.getElementById('demo').style.fontSize='35px'
+}
+
+function displayZero(){
+    document.getElementById('demo1').style.display='none'
+}
+
+function displayOn(){
+    document.getElementById('demo2').style.display='block'
+}
+
+function myFunction() {
+    document.getElementById('demo4').innerHTML = "Paragrafo changed."
+}
